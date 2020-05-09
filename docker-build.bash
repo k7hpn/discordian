215943dd8e47ad0651b9cf3de34b812057ffa9da
Harald Nagel <dev@hpn.dev>
2020-05-08 21:43:48 -0700

    Improve Docker build script

--- docker-build.bash
+++ docker-build.bash
@@ -7,9 +7,13 @@ readonly BLD_COMMIT=$(git rev-parse --short HEAD)
 readonly BLD_VERSION_DATE=$(date -u +'%Y%m%d_%H%M%SZ')
 readonly BLD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 
-BLD_PUSH=false
 BLD_RELEASE=false
 BLD_VERSION=unknown
+
+# Unset; just complete the bulid stage, otherwise complete the publish stage
+if [[ -z $BLD_PUBLISH ]]; then
+  BLD_PUBLISH=false
+fi
 
 # Branch information
 
@@ -50,18 +54,18 @@ fi
 if [[ $BLD_BRANCH = "master" ]]; then
   BLD_DOCKER_TAG="latest"
   BLD_VERSION=${BLD_BRANCH}-${BLD_VERSION_DATE}
-  BLD_PUSH=true
+  BLD_PUBLISH=true
   BLD_RELEASE=true
 elif [[ $BLD_BRANCH = "develop" ]]; then
   BLD_DOCKER_TAG="develop"
   BLD_VERSION=${BLD_BRANCH}-${BLD_VERSION_DATE}
-  BLD_PUSH=true
+  BLD_PUBLISH=true
 elif [[ "$BLD_BRANCH" =~ release/([0-9]+\.[0-9]+\.[0-9]+.*) ]]; then
   BLD_RELEASE_VERSION=${BASH_REMATCH[1]}
   BLD_DOCKER_TAG=v${BLD_RELEASE_VERSION}
   BLD_VERSION=v${BLD_RELEASE_VERSION}
   BLD_RELEASE=true
-  BLD_PUSH=true
+  BLD_PUBLISH=true
 else
   BLD_DOCKER_TAG=$BLD_COMMIT
   BLD_VERSION=${BLD_COMMIT}-${BLD_VERSION_DATE}
@@ -115,7 +119,7 @@ fi
 echo "=== Building branch $BLD_BRANCH commit $BLD_COMMIT as Docker image $BLD_FULL_DOCKER_IMAGE"
 echo "=== Image version: $BLD_VERSION"
 
-if [[ $BLD_PUSH = true ]]; then
+if [[ $BLD_PUBLISH = true ]]; then
   docker build -f "$BLD_DOCKERFILE" -t "$BLD_FULL_DOCKER_IMAGE" \
     --build-arg BRANCH="$BLD_BRANCH" \
     --build-arg IMAGE_CREATED="$BLD_DATE" \
@@ -170,3 +174,4 @@ if [[ $BLD_RELEASE = "true" && -f "release-publish.bash" ]]; then
       "$BLD_FULL_DOCKER_IMAGE"
   fi
 fi
+
